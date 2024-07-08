286fc2c8c7b490840dfae3cfd1dc491b744a473a
umardraz2004 <umardraz6965@gmail.com>
2024-07-08 15:06:17 +0500

    Website

--- InternshipProject/script/script.js
+++ InternshipProject/script/script.js
@@ -14,3 +14,10 @@ toggleBtnOpen.addEventListener('click', function(e) {
 toggleBtnClose.addEventListener('click', function(e) {
     appear.style.display = 'none';
 });
+
+const listItems = document.querySelectorAll('li');
+listItems.forEach(function (listItem) {
+    listItem.addEventListener('click', function (e) {
+        appear.style.display = 'none';
+    });
+});
